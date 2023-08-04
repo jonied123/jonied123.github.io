01ea85cb8e65ca2373014cf2742eb1c2b2734d4a
Edlejo <jonas.edler@grenzebach.com>
2023-08-04 13:27:48 +0200

    basic features are done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let dictionary = JSON.parse(localStorage.getItem('dictionary')) || {};
-
+let randomEnglishWord;
 
 function addVocabulary() {
     dictionary[englishText.value] = germanText.value;
@@ -7,10 +7,33 @@ function addVocabulary() {
     germanText.value = '';
 
     localStorage.setItem('dictionary', JSON.stringify(dictionary));
+    render();
 }
 
 function render() {
+    vocabularyList.innerHTML = '';
     for (let key in dictionary) {
-        vocabularyList.innerHTML += '<li>'+key+ ' | '+ dictionary[key]+'</li>'
+        vocabularyList.innerHTML += '<li>' + key + ' | ' + dictionary[key] + '</li>'
     }
+}
+
+function showVocabulary() {
+    //creates an arry with the keys of the JSON
+    let obj_keys = Object.keys(dictionary);
+    //random key gets saved
+    randomEnglishWord = obj_keys[Math.floor(Math.random() *obj_keys.length)];
+    EnglishWord.innerHTML= `${randomEnglishWord}?`;
+
+}
+
+function compare() {
+    if(inputGerman.value == dictionary[randomEnglishWord]){
+        inputGerman.innerHTML = '';
+        trueFalse.innerHTML = "Richtig!!";
+        showVocabulary();
+    }else{
+        //inputGerman.innerHTML = '';
+        trueFalse.innerHTML = "Falsch!!";
+    }
+
 }
